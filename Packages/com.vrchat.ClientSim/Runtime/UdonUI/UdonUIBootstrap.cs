8b0fb43cc21d26c482f17706f2ac7f245db47282
Talox <arne@vanderlei.be>
2022-10-21 03:22:45 +0200

    added updates

--- Packages/com.vrchat.ClientSim/Runtime/UdonUI/UdonUIBootstrap.cs
+++ Packages/com.vrchat.ClientSim/Runtime/UdonUI/UdonUIBootstrap.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 namespace VRC.UdonUI
 {
+    [ExecuteInEditMode]
     public class UdonUIBootstrap : MonoBehaviour
     {
-        public void Awake()
+
+        [DidReloadScripts(0)]
+        public static void OnDomainReload()
         {
             UdonUIGenerator.TMPRefrence = new TMPRefrence();
             UdonUIGenerator.CanvasRefrence = new CanvasGroupRefrence();
