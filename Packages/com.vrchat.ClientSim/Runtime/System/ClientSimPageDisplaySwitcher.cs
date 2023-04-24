8e224aad91bfde44594689408b57e90647f5c9b0
Talox <arne@vanderlei.be>
2023-04-24 16:59:57 +0200

    added udonUI panel changes

---
+++ Packages/com.vrchat.ClientSim/Runtime/System/ClientSimPageDisplaySwitcher.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static VRC.SDK3.ClientSim.ClientSimMenu;
+
+namespace VRC.SDK3.ClientSim {
+
+    public class ClientSimPageDisplaySwitcher : MonoBehaviour
+    {
+        [SerializeField]
+        private ClientSimMenu menu;
+
+        [SerializeField]
+        private ClientSimDisplayedPage page; 
+
+        public void OnButtonClicked()
+        {
+            menu.SetDisplayedPage(page);
+        }
+    }
+}
---
+++ Packages/com.vrchat.ClientSim/Runtime/System/ClientSimUdonUI.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRC.UdonUI.Interfaces;
+
+public class ClientSimUdonUI : MonoBehaviour, IQuickMenuReference
+{
+    private struct UdonUISortObject
+    {
+        public int index;
+        public RectTransform transform;
+    }
+
+    [SerializeField]
+    private RectTransform Content;
+
+    private List<UdonUISortObject> udonUISortObjects = new List<UdonUISortObject>();
+
+    public void AddPanelToMenu(RectTransform transform, int index)
+    {
+        transform.SetParent(Content, false);
+
+        udonUISortObjects.Add(new UdonUISortObject()
+        {
+            index = index,
+            transform = transform
+        });
+        Sort();
+    }
+
+    public Vector2 GetSizeOfPanel()
+    {
+        return Content.rect.size;
+    }
+
+    public void Sort()
+    {
+        udonUISortObjects.Sort((a, b) => a.index.CompareTo(b.index));
+
+        for (int i = 0; i < udonUISortObjects.Count; i++)
+        {
+            if (udonUISortObjects[i].transform != null)
+            {
+                udonUISortObjects[i].transform.SetAsLastSibling();
+            }
+            else
+            {
+                udonUISortObjects.RemoveAt(i);
+                i--;
+            }
+        }
+    }
+}
